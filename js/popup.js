8b390534ef5fdc72ef21d7f67cc462295b9787dc
BARANDA CABRERA GUSTAVO DANIEL <baranda.gustavo@gmail.com>
2022-03-14 20:30:26 -0300

    popup

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+const popup = document.querySelector('.popup');
+const close = document.querySelector('.close');
+
+window.onload = function(){
+    setTimeout(function(){
+        popup.style.display = "block";
+        
+    }, 2000)
+}
+
+close.addEventListener('click', () => {
+    popup.style.display = "none";
+});
